02df74afb7c562b79a1d1cac69ad1fd755f04858
Aloysius Gilang <gilangpramdya84@gmail.com>
2023-02-20 21:35:03 +0700

    add ambient temp display, add counter func

--- app/src/main/java/com/example/majika/fragment/Menu.kt
+++ app/src/main/java/com/example/majika/fragment/Menu.kt
@@ -1,25 +1,27 @@
 package com.example.majika.fragment
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.Parcelable
 import android.util.Log
-import android.view.Display
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.majika.R
-import com.example.majika.adapter.CabangAdapter
 import com.example.majika.adapter.MenuAdapter
-import com.example.majika.model.CabangRestoranModel
 import com.example.majika.retrofit.Retrofit
 import com.example.majika.retrofit.data.DataMenu
 import com.example.majika.retrofit.endpoint.MenuEndpoint
@@ -27,33 +29,66 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.io.IOException
 
 class Menu : Fragment() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
-    private lateinit var adapter: MenuAdapter
+    private  var adapter: MenuAdapter? = null
     private lateinit var recyclerView: RecyclerView
     private lateinit var searchView : SearchView
+    private lateinit var temperatureTextView: TextView
+    private var currentTemperature: Float = 0f
     private var menuList : ArrayList<DataMenu> = arrayListOf()
     private var recyclerViewState: Parcelable? = null
+    private val RECYCLER_VIEW_STATE_KEY = "recycler_view_state"
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        if (savedInstanceState != null) {
+            recyclerViewState = savedInstanceState.getParcelable(RECYCLER_VIEW_STATE_KEY)
+        }
 
         val view = inflater.inflate(R.layout.fragment_menu, container, false)
-        // SearchView
+        return view
+    }
+
+    // SAVING AND RESTORING STATE
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        recyclerViewState = recyclerView?.layoutManager?.onSaveInstanceState()
+        outState.putParcelable(RECYCLER_VIEW_STATE_KEY, recyclerViewState)
+        outState.putParcelable("recyclerViewState", recyclerView.layoutManager?.onSaveInstanceState())
+//        outState.putParcelableArrayList("menuItems", menuList)
+    }
+
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        if (savedInstanceState != null) {
+            recyclerViewState = savedInstanceState.getParcelable("recyclerViewState")
+        }
+            val items = savedInstanceState?.getParcelableArrayList<DataMenu>("menuItems")
+            if (items != null) {
+                menuList = items
+                adapter = MenuAdapter(menuList)
+                recyclerView.adapter = adapter
+            }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // temperature
+        temperatureTextView = view.findViewById(R.id.temp)
+
+        // searchView
         searchView = view.findViewById(R.id.search_view)
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -82,15 +117,16 @@ class Menu : Fragment() {
             }
         }
 
-        //RecylerView
+        //RecyclerView
         recyclerView = view.findViewById(R.id.menuRV)
-        recyclerView?.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
         //Restore state
         menuList = arrayListOf()
         if (savedInstanceState != null ){
             recyclerViewState = savedInstanceState.getParcelable("recyclerViewState")
-            recyclerView?.layoutManager?.onRestoreInstanceState(recyclerViewState)
+            recyclerViewState = savedInstanceState.getParcelable(RECYCLER_VIEW_STATE_KEY)
+            recyclerView.layoutManager?.onRestoreInstanceState(recyclerViewState)
 
 //            menuList = savedInstanceState.getParcelableArrayList("menuItems") ?: arrayListOf()
 //            adapter = MenuRvAdapter(menuList)
@@ -101,62 +137,16 @@ class Menu : Fragment() {
             }
         }
 
-
-
-        // Return View
-        return view
-    }
-
-    // SAVING AND RESTORING STATE
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putParcelable("recyclerViewState", recyclerView?.layoutManager?.onSaveInstanceState())
-//        outState.putParcelableArrayList("menuItems", menuList)
-    }
-
-
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
-        if (savedInstanceState != null) {
-            recyclerViewState = savedInstanceState.getParcelable("recyclerViewState")
-        }
-//            val items = savedInstanceState.getParcelableArrayList<MenuItem>("menuItems")
-//            if (items != null) {
-//                menuList = items
-//                adapter = MenuRvAdapter(menuList)
-//                recyclerView.adapter = adapter
-//            }
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        // handle counter button click
-
     }
 
     private suspend fun fetchMenuData() {
-////        val menuAPI = Retrofit.getInstance().create(MenuEndpoint::class.java)
-//        try {
-//            val response = menuAPI.getMenu().execute()
-//            if (response.isSuccessful) {
-//                val menu = response.body()?.data
-//                menuList = menu!!
-//                adapter = MenuAdapter(menu!!)
-//                recyclerView?.adapter = adapter
-//            } else {
-//                Log.d("error", response.errorBody()?.string() ?: "Unknown error")
-//            }
-//        } catch (e: IOException) {
-//            Log.d("error", e.message.toString())
-//        }
         val menuAPI = Retrofit.getInstance().create(MenuEndpoint::class.java)
         val operation = GlobalScope.async(Dispatchers.Default) {
             val response = menuAPI.getMenu()
             if (response != null) {
                 // Checking the result
                 Log.d("GetData", response.body().toString())
-                val dataMenu = response!!.body()!!.data
+                val dataMenu = response.body()!!.data
                 menuList = dataMenu
             }
         }
@@ -164,11 +154,45 @@ class Menu : Fragment() {
 
         adapter = MenuAdapter(menuList)
         recyclerView.adapter = adapter
-
-
     }
 
+    override fun onResume() {
+        super.onResume()
 
+        val sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val temperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+        if (temperatureSensor != null) {
+            sensorManager.registerListener(
+                temperatureListener,
+                temperatureSensor,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        } else {
+            temperatureTextView.text = "Temperature sensor not available"
+        }
+    }
 
+    override fun onPause() {
+        super.onPause()
+        val sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.unregisterListener(temperatureListener)
+    }
+
+    private val temperatureListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent?) {
+            if (event != null) {
+                currentTemperature = event.values[0]
+                updateTemperatureText()
+            }
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+            // do nothing
+        }
+    }
+
+    private fun updateTemperatureText() {
+        temperatureTextView.text = "Temp: ${currentTemperature}°C"
+    }
 
 }
--- app/src/main/java/com/example/majika/retrofit/data/DataMenu.kt
+++ app/src/main/java/com/example/majika/retrofit/data/DataMenu.kt
@@ -1,5 +1,7 @@
 package com.example.majika.retrofit.data
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class DataMenu (
@@ -10,4 +12,38 @@ data class DataMenu (
     @SerializedName("sold") val sold: Int,
     @SerializedName("type") val type: String,
     @SerializedName("count") var count: Int,
-    )
+    )  : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readDouble(),
+        parcel.readInt(),
+        parcel.readString().toString(),
+        parcel.readInt())
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(p0: Parcel, p1: Int) {
+        p0.writeString(name)
+        p0.writeString(description)
+        p0.writeString(currency)
+        p0.writeDouble(price)
+        p0.writeInt(sold)
+        p0.writeString(type)
+        p0.writeInt(count)
+    }
+
+    companion object CREATOR : Parcelable.Creator<DataMenu> {
+        override fun createFromParcel(parcel: Parcel): DataMenu {
+            return DataMenu(parcel)
+        }
+
+        override fun newArray(size: Int): Array<DataMenu?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
